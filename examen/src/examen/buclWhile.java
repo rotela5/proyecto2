35353dd2b13976c324d5159da6ea816757fe72a0
userrotela5git config --global user.email  rotramon@hotmail.comgit config --global user.name rotela5git remote add .git remote add origin https://github.com/rotela5/CRUD.gitgit push remoto mastergit remote add origin https://github.com/rotela5/crud1.gitgit add -am trabajo <rotramon@hotmail.com>
2020-11-30 08:27:33 -0300

    comit2

---
+++ examen/src/examen/buclWhile.java
@@ -0,0 +1,14 @@
+package examen;
+
+public class buclWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=1;
+while(n<100) {
+	System.out.println(n);
+	n++;
+}
+	}
+
+}
---
+++ examen/src/examen/inroduceNombre.java
@@ -0,0 +1,15 @@
+package examen;
+
+import java.util.Scanner;
+
+public class inroduceNombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner teclado=new Scanner(System.in);
+System.out.println("introduce tu nombre ");
+String nombre=teclado.nextLine();
+System.out.println("tu nombe es :"+nombre);
+	}
+
+}
